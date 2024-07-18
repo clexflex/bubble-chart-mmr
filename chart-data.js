bea2437de507dabbe3769bb4c114a55698bf1dc4
yashraj <yashrajjghosalkar@gmail.com>
2024-07-18 17:42:11 +0530

    version 2.9

---
+++ chart-data.js
@@ -0,0 +1,134 @@
+const chartData = {
+    category1: [
+        {label: 'Speed Sensor Market', size2023: 9.63, size2030: '13.37 Bn', CAGR: 4.80, url: 'https://www.stellarmr.com/report/Speed-Sensor-Market/200'},
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://www.stellarmr.com/report/Public-Safety-Sensors-Market/1404'},
+        {label: 'Biomedical Temperature Sensor Market', size2023: 12.84, size2030: '20.61 Bn', CAGR: 7, url: 'https://www.stellarmr.com/report/Biomedical-Temperature-Sensor-Market/1449'},
+        {label: 'Artificial Intelligence Sensor Market', size2023: 4.06, size2030: '55.62 Bn', CAGR: 45.30, url: 'https://www.stellarmr.com/report/Artificial-Intelligence-Sensor-Market/1506'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+        {label: 'Level Sensor Market', size2023: 4.64, size2030: '7.98 Bn', CAGR: 8.05, url: 'https://www.stellarmr.com/report/Level-Sensor-Market/2238'}
+    ],
+    category2: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category3: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category4: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category5: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category6: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category7: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category8: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category9: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category10: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category11: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category12: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category13: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category14: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category15: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    category16: [
+        {label: 'Public Safety Sensors Market', size2023: 1.81, size2030: '2.87 Bn', CAGR: 6.80, url: 'https://example.com/public-safety-sensors-market'},
+        {label: 'Automotive Sensor Market', size2023: 13.5, size2030: '23.44 Bn', CAGR: 8.20, url: 'https://www.stellarmr.com/report/Automotive-Sensor-Market/1857'},
+        {label: 'Industrial Sensors Market', size2023: 25.87, size2030: '44.42 Bn', CAGR: 8.03, url: 'https://www.stellarmr.com/report/Industrial-Sensors-Market/2197'},
+        {label: 'Infrared Sensors Market', size2023: 1.21, size2030: '2.71 Bn', CAGR: 12.20, url: 'https://www.stellarmr.com/report/Infrared-Sensors-Market/2198'},
+        {label: 'Smartphone Sensors Market', size2023: 82.96, size2030: '269.01 Bn', CAGR: 18.30, url: 'https://www.stellarmr.com/report/Smartphone-Sensors-Market/2199'},
+       
+    ],
+    
+};
